6a283dc5e710a1f7682c5e08ea784cb21147aca8
riveleus <riveleus@gmail.com>
2024-10-20 08:26:16 +0700

    Revert "fix bug host value"
    This reverts commit 299523e363e713a3b87d2f64e850fe386fe9db52.

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import { routes } from './routes';
 const init = async () => {
     const server = Hapi.server({
         port: 5000,
-        host: process.env.NODE_ENV !==  'production' ? 'localhost' : '0.0.0.0',
+        host: 'localhost',
         routes: {
             cors: {
                 origin: ['*'],
