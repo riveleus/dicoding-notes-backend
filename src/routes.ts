ff5e23ddac10bbabbc5842409eb3cbf0f89d8d5e
riveleus <riveleus@gmail.com>
2024-10-20 08:05:23 +0700

    initial commit

---
+++ src/routes.ts
@@ -0,0 +1,30 @@
+import { ServerRoute } from "@hapi/hapi";
+import { addNoteHandler, deleteNoteByIdHandler, editNoteByIdHandler, getAllNotesHandler, getNoteByIdHandler } from "./handler";
+
+export const routes: ServerRoute[] = [
+    {
+        method: 'POST',
+        path: '/notes',
+        handler: addNoteHandler,
+    },
+    {
+        method: 'GET',
+        path: '/notes',
+        handler: getAllNotesHandler,
+    },
+    {
+        method: 'GET',
+        path: '/notes/{id}',
+        handler: getNoteByIdHandler,
+    },
+    {
+        method: 'PUT',
+        path: '/notes/{id}',
+        handler: editNoteByIdHandler,
+    },
+    {
+        method: 'DELETE',
+        path: '/notes/{id}',
+        handler: deleteNoteByIdHandler,
+    },
+];
